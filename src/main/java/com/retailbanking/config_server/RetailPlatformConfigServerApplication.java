f90003a976cfa6c81de20c6fafa6f8ad675b3119
Bhaskar Dasari <bhaskardasari3004@gmail.com>
2026-05-26 21:41:46 +0400

    configs added

---
+++ src/main/java/com/retailbanking/config_server/RetailPlatformConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.retailbanking.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailPlatformConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailPlatformConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retailbanking/config_server/RetailPlatformConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailbanking.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailPlatformConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
